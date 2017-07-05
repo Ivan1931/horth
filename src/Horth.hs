0268a75b781af73f01290d11ca23c2d1e9fdda04
jonah-hooper <jonah.graham.hooper@gmail.com>
2017-07-05 22:11:40 +0200

    Refactored code to remove some superfelous errors. Added a bunch of way better names and naming conventions. Created a lookup system with lookup tests. Unified all word storage into a single dictionary.

--- src/Horth.hs
+++ src/Horth.hs
@@ -6,45 +6,50 @@ import           Control.Exception    (Exception, catch)
 import           Control.Monad.Except (ExceptT, runExceptT, throwError)
 import           Control.Monad.State  (StateT, get, put, runStateT)
 import           Control.Monad.Writer (MonadWriter, tell)
+import           Control.Monad (when)
 import           Data.Default         (Default, def)
-import           Data.Map             (Map, fromList)
+import           Data.Map             (Map, fromList, lookup, insert)
+import           Prelude hiding (lookup)
 
 data Value =
   Number Int | -- represents a number
   String String -- Characters delimited by ""
   deriving (Eq, Show, Read)
 
-data Forth =
+data Instruction =
   Value Value | -- Basic value
   Word String | -- Just a sequence of characters that represents forth word
-  Block Program -- : sentence started with a sequence of colons and ended with ;
+  Definition Program | -- : sentence started with a sequence of colons and ended with ;
+  Loop Program | -- Represents a loop structure of the forth program
+  If Program Program -- Represents the branches of an if - else conditional
   deriving (Eq, Show)
 
-type Program = [Forth]  -- for now represent programs as list of forth words
+type Program = [Instruction]  -- for now represent programs as list of Instruction words
 type Stack = [Value] -- for now represent the stack as a list
-type StackOp = Stack -> Stack -- for now represent stack as function on top of stack
-type Constants = Map String Value
-type Variables = Map String Value
+
+data DictionaryEntry =
+  Constant Value | -- Wrapper for storing a constant a variable that can't be modified
+  Variable Value | -- Wrapper for storing a variable which can be changed
+  WordEntry Program -- Wrapper for a reference to a block that is both mutable
+  deriving (Eq, Show)
 
 data ForthState = ForthState {
-  constants :: Constants,
-  variables :: Variables,
-  stack     :: Stack,
-  program   :: Program
+  dictionary :: Map String DictionaryEntry,
+  stack      :: Stack,
+  program    :: Program
 } deriving (Eq, Show)
 
 instance Default ForthState where
   def = ForthState {
-    constants = [],
-    variables = [],
-    stack = [],
-    program = []
+    dictionary = [],
+    stack      = [],
+    program    = []
   }
 
 data ForthError = ParseError
-                | StackUnderflow  -- Try to pop something off an empty stack
-                | Stackoverflow  -- We have exceeded the stack size (impossible since this is haskell)
-                | EmptyInstructionStack -- We don't have any instructions left to do
+                | WordNotFound String -- Happens when attempting to lookup a word that does not exist
+                | StackUnderflow  -- Happens when pop is called on empty stack
+                | EmptyInstructionStack -- Happens when attempting to get next instruction on null program
                 deriving (Show, Eq)
 
 instance Exception ForthError
@@ -87,15 +92,15 @@ hasInstruction = do
     [] -> False
     _  -> True
 
-nextInstruction :: Interpreter m Forth
+nextInstruction :: Interpreter m Instruction
 nextInstruction = do
   state <- get
   case program state of
     [] -> throwError EmptyInstructionStack
     (x:xs) -> return x
 
-popInstructionStack :: Interpreter m Forth
-popInstructionStack = do
+popInstruction :: Interpreter m Instruction
+popInstruction = do
   state <- get
   case program state of
     [] -> throwError EmptyInstructionStack
@@ -103,15 +108,52 @@ popInstructionStack = do
       put state { program = [] }
       return x
 
+push :: Value -> Interpreter m ()
+push value = undefined
+
+pushInstruction :: Instruction -> Interpreter m ()
+pushInstruction i = do
+  state <- get
+  put $ state { program = i : program state }
+
+lookupWord :: String -> Interpreter m DictionaryEntry
+lookupWord s = do
+  values <- dictionary <$> get
+  case lookup s values of
+    Just instructions -> return instructions
+    Nothing           -> throwError $ WordNotFound s
+
 type InterpreterResult a = Either ForthError (a, ForthState)
 
 evaluateInterpreter :: FIO m => Interpreter m a -> ForthState -> m (InterpreterResult a)
 evaluateInterpreter i state = runExceptT (runStateT i state)
 
-interpreter :: Interpreter m Value
-interpreter = pop
+interpretWord :: String -> Interpreter m ()
+interpretWord word = do
+  word <- lookupWord word
+  case word of
+    Constant  constant -> push constant
+    Variable  variable -> push variable
+    WordEntry program  -> evaluateProgram program
 
-evaluateForth :: FIO m => ForthState -> m (InterpreterResult Value)
+evaluateInstruction :: Instruction -> Interpreter m ()
+evaluateInstruction = undefined
+
+evaluateProgram :: Program -> Interpreter m ()
+evaluateProgram = undefined
+
+interpreter :: Interpreter m ()
+interpreter = do
+  continue <- hasInstruction
+  when continue $ do
+    i <- popInstruction
+    case i of
+      Value value           -> push value
+      Word word             -> interpretWord word
+      Definition definition -> evaluateProgram definition
+    interpreter
+
+evaluateForth :: FIO m => ForthState -> m (InterpreterResult ())
 evaluateForth = evaluateInterpreter interpreter
 
 repl :: IO ()
--- test/Horth/InterpreterSpec.hs
+++ test/Horth/InterpreterSpec.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE RankNTypes, FlexibleContexts #-}
+{-# LANGUAGE OverloadedLists, RankNTypes, FlexibleContexts #-}
 module Horth.InterpreterSpec where
 
 import Test.Hspec
@@ -11,10 +11,13 @@ spec = describe "Interpreter" $ do
   popSpec
   emitSpec
   instructionSpec
+  lookupSpec
 
 type TestInterpreter a = Interpreter (Writer [Value]) a
 
-runTestInterpreter :: TestInterpreter a -> ForthState -> (InterpreterResult a, [Value])
+runTestInterpreter :: TestInterpreter a ->
+                      ForthState ->
+                      (InterpreterResult a, [Value])
 runTestInterpreter i s = runWriter $ evaluateInterpreter i s
 
 emitSpec :: Spec
@@ -28,7 +31,7 @@ emitSpec =
         emitted `shouldBe` [Number 1]
 
 instructionSpec :: Spec
-instructionSpec =
+instructionSpec = do
   describe "hasInstruction" $
     let
       testWord = Word "hello"
@@ -37,17 +40,23 @@ instructionSpec =
       (result, _) = runTestInterpreter hasInstruction testProgram
       (resultE, _) = runTestInterpreter hasInstruction testProgramEmpty
       (resultN, _) = runTestInterpreter nextInstruction testProgram
-      (resultNextPopped, _) = runTestInterpreter popInstructionStack testProgram
+      (resultNextPopped, _) = runTestInterpreter popInstruction testProgram
     in do
-      it "correctly checks that a program is non-empty" $
+      it "checks that a program is non-empty" $
         result `shouldBe` Right (True, testProgram)
-      it "correctly checks that a program is empty" $
+      it "checks that a program is empty" $
         resultE `shouldBe` Right (False, testProgramEmpty)
-      it "correctly lifts the next test program off the stack" $
+      it "lifts the next test program off the stack" $
         resultN `shouldBe` Right (testWord, testProgram)
-      it "correctly pops the instruction stack and returns the popped instruction" $
+      it "pops the instruction stack and returns the popped instruction" $
         resultNextPopped `shouldBe` Right (testWord, def)
-
+  describe "pushInstruction" $
+    let
+      testWord = Word "hello"
+      nonEmptyProgram = def { program = [testWord] }
+      (result, _) = runTestInterpreter (pushInstruction testWord) def
+    in it "pushes a word onto the stack" $
+      result `shouldBe` Right ((), nonEmptyProgram)
 
 popSpec :: Spec
 popSpec =
@@ -66,3 +75,20 @@ popSpec =
           let
             (result, _) = runTestInterpreter pop def
           in result `shouldBe` Left StackUnderflow
+
+lookupSpec :: Spec
+lookupSpec = describe "lookupWord" $
+  let
+    word1 = "word"
+    value1 = Number 1
+    testState = def { dictionary = [(word1, Variable value1)]}
+  in do
+    it "looks up a variable and does not change the stack" $
+      let
+        (result, _) = runTestInterpreter (lookupWord word1) testState
+      in result `shouldBe` Right (Variable value1, testState)
+    it "throws a word not found error when the variable is not found" $
+      let
+        nonExistantWord = "notexistant"
+        (notFoundResult, _) = runTestInterpreter (lookupWord nonExistantWord) testState
+      in notFoundResult `shouldBe` Left (WordNotFound nonExistantWord)
